6e91a9e7afc6800b98fe0934869c2c2e0b758998
d-haru <halmuda6231@gmail.com>
2024-03-11 15:56:29 +0900

    workflow_sync.shを追加

---
+++ workflow_sync.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+URL="https://api.github.com/repos/hatena/Hatena-Blog-Workflows-Boilerplate/contents/.github/workflows"
+FILES=$(curl -fsSL "$URL")
+
+for row in $(echo "${FILES}" | jq -r '.[] | @base64'); do
+    json=$(echo ${row} | base64 --decode)
+    download_url=$(echo ${json} | jq -r '.download_url')
+    path=$(echo ${json} | jq -r '.path')
+
+    curl -fsSL -o "$path" "$download_url"
+done
