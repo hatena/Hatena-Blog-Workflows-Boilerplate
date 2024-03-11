602c25e005ee6666ea28cf4f05b4f9f500bb9e0e
d-haru <halmuda6231@gmail.com>
2024-03-11 16:11:45 +0900

    shellチェック修正

--- scripts/workflow_sync.sh
+++ scripts/workflow_sync.sh
@@ -5,9 +5,9 @@ URL="https://api.github.com/repos/hatena/Hatena-Blog-Workflows-Boilerplate/conte
 FILES=$(curl -fsSL "$URL")
 
 for row in $(echo "${FILES}" | jq -r '.[] | @base64'); do
-    json=$(echo ${row} | base64 --decode)
-    download_url=$(echo ${json} | jq -r '.download_url')
-    path=$(echo ${json} | jq -r '.path')
+    json=$(echo "${row}" | base64 --decode)
+    download_url=$(echo "${json}" | jq -r '.download_url')
+    path=$(echo "${json}" | jq -r '.path')
 
     curl -fsSL -o "$BASE_DIR/$path" "$download_url"
 done
